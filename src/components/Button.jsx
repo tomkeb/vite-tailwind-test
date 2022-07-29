039baf34055996c1c993de77e99304c19205ae3a
tomkeb <tomaskeb@gmail.com>
2022-07-29 23:45:41 +0200

    working buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
 const buttonInfo = {
-  class: "bg-slate-200 hover:bg-slate-300 rounded-xl p-3"
+  class: "bg-slate-200 hover:bg-slate-300 rounded-xl p-2 m-1 border-2 border-slate-300 hover:border-indigo-500"
 };
 
 
 export default function Button(props) {
   return (
     <>
-      <button className={buttonInfo.class}>{props.buttonText}</button>
+      <button className={buttonInfo.class} onClick={props.onClick}>{props.buttonText}</button>
     </>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,15 +3,27 @@ import { useState } from "react";
 
 export default function Counter() {
   const [counter, setCounter] = useState(0);
+
+  function counterAdd() {
+    setCounter((prevCount) => prevCount + 10);
+  }
+
+  function counterSubstract() {
+    setCounter((prevCount) => prevCount - 10);
+  }
+
+  function counterReset() {
+    setCounter(0);
+  }
+
   return (
     <>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
-        minus
-      </button>
-      <h1>{counter}</h1>
+      <Button onClick={counterAdd} buttonText="add" />
+      <Button onClick={counterSubstract} buttonText="substract" />
+      <Button onClick={counterReset} buttonText="reset" />
+      <h1 class="rounded-full text-center w-16 h-16 bg-slate-800 text-white mx-auto leading-[4rem]">
+        {counter}
+      </h1>
     </>
   );
 }
